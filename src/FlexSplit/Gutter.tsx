9e4facabc893413539a187bfade38846b9e52303
Roy Chen <roy850323@gmail.com>
2023-05-31 23:19:25 +0800

    Split flex logic

---
+++ src/FlexSplit/Gutter.tsx
@@ -0,0 +1,193 @@
+import { useState, useEffect, useRef } from 'react';
+import { GutterProps, MousePosition, SiblingInfo, Direction } from './types';
+import { getStyleKey, getSiblingSizes } from './utils';
+
+const defaultMousePosition: MousePosition = {
+  x: 0,
+  y: 0,
+};
+const defaultSiblingElement: SiblingInfo = {
+  startRect: null,
+};
+
+const Gutter = ({
+  index,
+  size = 10,
+  style,
+  direction = 'horizontal',
+  minItemSizes,
+  itemSizes,
+  onGutterDown,
+  onGutterMove,
+  onGutterUp,
+  ...props
+}: GutterProps) => {
+  const gutterRef = useRef<HTMLDivElement>(null);
+  const previousSiblingInfoRef = useRef<SiblingInfo>(defaultSiblingElement);
+  const nextSiblingInfoRef = useRef<SiblingInfo>(defaultSiblingElement);
+  const mouseDownPositionRef = useRef<MousePosition>(defaultMousePosition);
+  const [mouseDown, setMouseDown] = useState(false);
+  const styleKey = getStyleKey(direction);
+
+  const onMouseDown = (event: React.MouseEvent) => {
+    event.preventDefault();
+    if (!gutterRef.current) return;
+
+    setMouseDown(true);
+    mouseDownPositionRef.current = {
+      x: event.clientX,
+      y: event.clientY,
+    };
+
+    if (gutterRef.current.previousSibling instanceof HTMLElement) {
+      const rect = gutterRef.current.previousSibling.getBoundingClientRect();
+      previousSiblingInfoRef.current = {
+        startRect: rect,
+      };
+    }
+    if (gutterRef.current.nextSibling instanceof HTMLElement) {
+      const rect = gutterRef.current.nextSibling.getBoundingClientRect();
+      nextSiblingInfoRef.current = {
+        startRect: rect,
+      };
+    }
+    onGutterDown?.(event.nativeEvent);
+  };
+
+  useEffect(() => {
+    const flexMode = (
+      moveDistance: number,
+      callback: (siblingItemPixelSizes: number[], moveDistance: number) => void
+    ) => {
+      if (
+        !gutterRef.current ||
+        !previousSiblingInfoRef.current.startRect ||
+        !nextSiblingInfoRef.current.startRect
+      ) {
+        return;
+      }
+
+      const currentSiblingSizes = getSiblingSizes(itemSizes, index);
+
+      // calculate new size
+      const newPreviousSiblingPixelSize =
+        previousSiblingInfoRef.current.startRect[styleKey] + moveDistance;
+      const newNextSiblingPixelSize =
+        nextSiblingInfoRef.current.startRect[styleKey] - moveDistance;
+
+      // calculate min size
+      const adjustPreviousGutterMinSize = index === 0 ? size / 2 : size;
+      const adjustNextGutterMinSize =
+        index + 2 === itemSizes.length ? size / 2 : size;
+      const siblingMinSizes = getSiblingSizes(minItemSizes ?? [], index);
+      const previousMinPixelSize =
+        (siblingMinSizes[0] ?? 0) + adjustPreviousGutterMinSize;
+      const nextMinPixelSize =
+        (siblingMinSizes[1] ?? 0) + adjustNextGutterMinSize;
+
+      // set new size
+      const newPreviousSizeBiggerMinSize =
+        newPreviousSiblingPixelSize >= previousMinPixelSize;
+      const newNextSizeBiggerMinSize =
+        newNextSiblingPixelSize >= nextMinPixelSize;
+      if (newPreviousSizeBiggerMinSize && newNextSizeBiggerMinSize) {
+        const newSiblingPixelItemSizes = [
+          newPreviousSiblingPixelSize,
+          newNextSiblingPixelSize,
+        ];
+        callback(newSiblingPixelItemSizes, moveDistance);
+      }
+
+      // fix size
+      // const currentPreviousSiblingPixelSize = currentSiblingSizes[0] ?? 0;
+      // const currentNextSiblingPixelSize = currentSiblingSizes[1] ?? 0;
+      const currentPreviousSiblingPixelSize = previousSiblingInfoRef.current.startRect[styleKey];
+      const currentNextSiblingPixelSize = nextSiblingInfoRef.current.startRect[styleKey];
+      const needFixPreviousPxSize =
+        newPreviousSiblingPixelSize !== currentPreviousSiblingPixelSize &&
+        newPreviousSiblingPixelSize <= previousMinPixelSize &&
+        currentPreviousSiblingPixelSize > previousMinPixelSize;
+      if (needFixPreviousPxSize) {
+        const fixPreviousSizePx = previousMinPixelSize;
+        const fixNextSizePx =
+          currentNextSiblingPixelSize +
+          currentPreviousSiblingPixelSize -
+          previousMinPixelSize;
+        const newSiblingPixelItemSizes = [fixPreviousSizePx, fixNextSizePx];
+        callback(newSiblingPixelItemSizes, moveDistance);
+      }
+      const needFixNextPxSize =
+        newNextSiblingPixelSize !== currentNextSiblingPixelSize &&
+        newNextSiblingPixelSize <= nextMinPixelSize &&
+        currentNextSiblingPixelSize > nextMinPixelSize;
+      if (needFixNextPxSize) {
+        const fixPreviousSizePx =
+          currentPreviousSiblingPixelSize +
+          currentNextSiblingPixelSize -
+          nextMinPixelSize;
+        const fixNextSizePx = nextMinPixelSize;
+        const newSiblingPixelItemSizes = [fixPreviousSizePx, fixNextSizePx];
+        callback(newSiblingPixelItemSizes, moveDistance);
+      }
+    };
+    const onMouseMove = (event: MouseEvent) => {
+      event.preventDefault();
+      if (direction === 'horizontal') {
+        const moveDistance = event.clientX - mouseDownPositionRef.current.x;
+        flexMode(moveDistance, (siblingItemPixelSizes) => {
+          onGutterMove?.(siblingItemPixelSizes, event);
+        });
+      }
+      if (direction === 'vertical') {
+        const moveDistance = event.clientY - mouseDownPositionRef.current.y;
+        flexMode(moveDistance, (siblingItemPixelSizes) => {
+          onGutterMove?.(siblingItemPixelSizes, event);
+        });
+      }
+    };
+    const onMouseUp = (event: MouseEvent) => {
+      event.preventDefault();
+      setMouseDown(false);
+      onGutterUp?.(event);
+    };
+
+    if (mouseDown) {
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    }
+
+    return () => {
+      if (mouseDown) {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      }
+    };
+  }, [mouseDown, direction, minItemSizes, itemSizes]);
+
+  const getGutterClassName = () => {
+    let className = 'gutter';
+    if (direction === 'horizontal') {
+      className += ' gutter--horizontal';
+    }
+    if (direction === 'vertical') {
+      className += ' gutter--vertical';
+    }
+    return className;
+  };
+  const gutterClassName = getGutterClassName();
+
+  return (
+    <div
+      {...props}
+      ref={gutterRef}
+      style={{
+        ...style,
+        [styleKey]: `${size}px`,
+      }}
+      className={gutterClassName}
+      onMouseDown={onMouseDown}
+    ></div>
+  );
+};
+
+export default Gutter;
---
+++ src/FlexSplit/index.tsx
@@ -0,0 +1,246 @@
+import { useState, useEffect, useRef, useCallback, Fragment } from 'react';
+import Gutter from './Gutter';
+import { SplitProps } from './types';
+import {
+  formatItemSizes,
+  checkSizeRange,
+  getStyleKey,
+  toPercent,
+} from './utils';
+import './styles.css';
+
+const Split = ({
+  children,
+  direction = 'horizontal',
+  minItemSizes: outsideMinItemSizes = [],
+  // itemSizes: outsideItemSizes,
+  gutterSize = 10,
+  gutterStyle,
+  onGutterDown,
+  onGutterMove,
+  onGutterUp,
+  ...props
+}: SplitProps) => {
+  const getSplitClassName = (): string => {
+    let className = 'split';
+    if (direction === 'horizontal') {
+      className += ' split--horizontal';
+    }
+
+    if (direction === 'vertical') {
+      className += ' split--vertical';
+    }
+    className += ' split--flex-container';
+
+    return className;
+  };
+  const splitClassName = getSplitClassName();
+
+  if (children instanceof Array) {
+    const minItemSizes = formatItemSizes(outsideMinItemSizes, children.length);
+    const [innerItemSizes, setInnerItemSizes] = useState<number[]>([]);
+    const [percentItemSizes, setPercentItemSizes] = useState<number[]>([]);
+    const [percentStringItemSizes, setPercentStringItemSizes] = useState<
+      string[]
+    >([]);
+    const itemSizes = innerItemSizes;
+    const styleKey = getStyleKey(direction);
+    const splitRef = useRef<HTMLDivElement>(null);
+
+    const calculatePercentItemSizes = (pixelItemSizes: number[]): number[] => {
+      const totalPixelItemSize = pixelItemSizes.reduce(
+        (accumulator, a) => accumulator + a,
+        0
+      );
+      const percentItemSizes = pixelItemSizes.map((pixelSize) =>
+        toPercent(pixelSize, totalPixelItemSize)
+      );
+      return percentItemSizes;
+    };
+
+    const updateItemSizes = useCallback(
+      (pixelItemSizes: number[]) => {
+        if (!splitRef.current) return;
+
+        const roundPixelItemSizes = pixelItemSizes.map((size) =>
+          Math.round(size)
+        );
+        setInnerItemSizes(roundPixelItemSizes);
+        const percentItemSizes = calculatePercentItemSizes(roundPixelItemSizes);
+        setPercentItemSizes(percentItemSizes);
+        const percentStringItemSizes = percentItemSizes.map(
+          (percentSize, percentSizeIdx) => {
+            if (
+              percentSizeIdx === 0 ||
+              percentSizeIdx + 1 === percentItemSizes.length
+            ) {
+              return `calc(${percentSize}% - ${gutterSize / 2}px`;
+            }
+
+            return `calc(${percentSize}% - ${gutterSize}px`;
+          }
+        );
+        setPercentStringItemSizes(percentStringItemSizes);
+      },
+      [gutterSize]
+    );
+
+    const getSplitItemSizes = (): number[] => {
+      if (splitRef.current) {
+        // auto fill split item size
+        const splitItemElements =
+          splitRef.current.querySelectorAll('.split__item');
+        const splitItemRects = Array.from(splitItemElements).map(
+          (splitItemElement) => {
+            return splitItemElement.getBoundingClientRect();
+          }
+        );
+        const pixelItemSizes = splitItemRects.map((rect) => rect[styleKey]);
+        return pixelItemSizes;
+      }
+      return [];
+    };
+
+    // set mount size
+    useEffect(() => {
+      if (splitRef.current) {
+        // auto fill split item size
+        const splitItemElements =
+          splitRef.current.querySelectorAll('.split__item');
+        splitItemElements.forEach((splitItemElement) => {
+          if (splitItemElement instanceof HTMLElement) {
+            splitItemElement.style.flex = '1';
+          }
+        });
+        const splitItemRects = Array.from(splitItemElements).map(
+          (splitItemElement) => {
+            return splitItemElement.getBoundingClientRect();
+          }
+        );
+        const splitItemSizes = splitItemRects.map((rect) => rect[styleKey]);
+        splitItemElements.forEach((splitItemElement) => {
+          if (splitItemElement instanceof HTMLElement) {
+            splitItemElement.style.flex = '';
+          }
+        });
+        updateItemSizes(splitItemSizes);
+      }
+    }, [updateItemSizes]);
+
+    return (
+      <div
+        {...props}
+        ref={splitRef}
+        className={`${splitClassName}${
+          props.className ? ` ${props.className}` : ''
+        }`}
+      >
+        {children.map((eachChild, childIdx) => {
+          const checkedSize = checkSizeRange(
+            minItemSizes[childIdx],
+            itemSizes[childIdx]
+          );
+          const renderSize = percentStringItemSizes[childIdx];
+          const splitItemStyle = {
+            [styleKey]: renderSize,
+          };
+
+          if (childIdx + 1 === children.length) {
+            return (
+              <div
+                key={childIdx}
+                className="split__item"
+                style={splitItemStyle}
+              >
+                {eachChild}
+              </div>
+            );
+          }
+
+          return (
+            <Fragment key={childIdx}>
+              <div className="split__item" style={splitItemStyle}>
+                {eachChild}
+              </div>
+              <Gutter
+                index={childIdx}
+                size={gutterSize}
+                style={gutterStyle}
+                direction={direction}
+                minItemSizes={minItemSizes}
+                itemSizes={itemSizes}
+                onGutterDown={(event) => {
+                  if (splitRef.current) {
+                    const totalSplitSize =
+                      splitRef.current.getBoundingClientRect()[styleKey];
+
+                    // render_size = total_size * percent_item_size - gutter_size
+
+                    // const newItemSizes = getSplitItemSizes().map(
+                    //   (itemSize, itemSizeIdx) => {
+                    //     console.log(itemSize);
+                    //     if (
+                    //       itemSizeIdx === 0 ||
+                    //       itemSizeIdx + 1 === children.length
+                    //     ) {
+                    //       return (
+                    //         ((itemSize + gutterSize / 2) / totalSplitSize) * 100
+                    //       );
+                    //     }
+                    //     return ((itemSize + gutterSize) / totalSplitSize) * 100;
+                    //   }
+                    // );
+
+                    const newItemSizes = percentItemSizes.map((percentSize) => {
+                      const result =
+                        ((totalSplitSize - (children.length - 1) * gutterSize) *
+                          percentSize) /
+                        100;
+                      return Math.round(result);
+                    });
+
+                    updateItemSizes(newItemSizes);
+                  }
+                  onGutterDown?.(itemSizes, event);
+                }}
+                onGutterMove={(newSiblingPixelItemSizes, event) => {
+                  const newPixelItemSizes = itemSizes.map(
+                    (itemSize, itemSizeIdx) => {
+                      if (childIdx === itemSizeIdx) {
+                        return newSiblingPixelItemSizes[0];
+                      }
+                      if (childIdx + 1 === itemSizeIdx) {
+                        return newSiblingPixelItemSizes[1] ?? itemSize;
+                      }
+                      return itemSize;
+                    }
+                  );
+                  updateItemSizes(
+                    newPixelItemSizes.map((size) => Math.round(size))
+                  );
+                  onGutterMove?.(newPixelItemSizes, event);
+                }}
+                onGutterUp={(event) => {
+                  onGutterUp?.(itemSizes, event);
+                }}
+              />
+            </Fragment>
+          );
+        })}
+      </div>
+    );
+  }
+
+  return (
+    <div
+      {...props}
+      className={`${splitClassName}${
+        props.className ? ` ${props.className}` : ''
+      }`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Split;
