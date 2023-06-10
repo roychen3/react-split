d4036b2a7d5eeba6cfec570b6020a229ecefc747
Roy Chen <roy850323@gmail.com>
2023-06-10 11:59:17 +0800

    common utils

--- src/Split/FixedSplit/utils.ts
+++ src/Split/FixedSplit/utils.ts
@@ -1,36 +1,11 @@
-import { Direction, ItemSizes } from './types';
+import {
+  isNumber,
+  getStyleKey,
+  getSiblingSizes,
+  formatItemSizes,
+} from '../utils';
 
-export const isNumber = (value: any): value is number =>
-  typeof value === 'number';
-
-export const getStyleKey = (direction: Direction): 'width' | 'height' => {
-  switch (direction) {
-    case 'horizontal':
-      return 'width';
-
-    case 'vertical':
-      return 'height';
-  }
-};
-
-export const getSiblingSizes = (
-  sizes: number | number[],
-  idx?: number
-): [null | number, null | number] => {
-  if (isNumber(sizes)) {
-    return [sizes, sizes];
-  }
-
-  const arraySizes = sizes instanceof Array;
-  if (arraySizes && isNumber(idx)) {
-    const previousSize = sizes[idx] ?? null;
-    const nextSize = sizes[idx + 1] ?? null;
-
-    return [previousSize, nextSize];
-  }
-
-  return [null, null];
-};
+export { isNumber, getStyleKey, getSiblingSizes, formatItemSizes };
 
 export const checkSizeRange = (
   minSize: number | null,
@@ -43,27 +18,6 @@ export const checkSizeRange = (
     return size;
   }
   return null;
-};
-
-export const formatItemSizes = (
-  itemSizes: ItemSizes,
-  length: number
-): number[] => {
-  if (isNumber(itemSizes)) {
-    return Array.from({ length }, () => itemSizes);
-  }
-  if (itemSizes instanceof Array) {
-    if (itemSizes.length === 0) {
-      return Array.from({ length }, () => 0);
-    }
-    if (itemSizes.length < length) {
-      return Array.from({ length }, () => 0).map((_, idx) => {
-        return itemSizes[idx] ?? 0;
-      });
-    }
-    return itemSizes;
-  }
-  return [];
 };
 
 export const fillItemSizes = (
--- src/Split/FlexSplit/utils.ts
+++ src/Split/FlexSplit/utils.ts
@@ -1,57 +1,6 @@
-import { Direction, ItemSizes } from './types';
+import { getStyleKey, getSiblingSizes, formatItemSizes } from '../utils';
 
-export const isNumber = (value: any): value is number =>
-  typeof value === 'number';
-
-export const getStyleKey = (direction: Direction): 'width' | 'height' => {
-  switch (direction) {
-    case 'horizontal':
-      return 'width';
-
-    case 'vertical':
-      return 'height';
-  }
-};
-
-export const getSiblingSizes = (
-  sizes: number | number[],
-  idx?: number
-): [null | number, null | number] => {
-  if (isNumber(sizes)) {
-    return [sizes, sizes];
-  }
-
-  const arraySizes = sizes instanceof Array;
-  if (arraySizes && isNumber(idx)) {
-    const previousSize = sizes[idx] ?? null;
-    const nextSize = sizes[idx + 1] ?? null;
-
-    return [previousSize, nextSize];
-  }
-
-  return [null, null];
-};
-
-export const formatItemSizes = (
-  itemSizes: ItemSizes,
-  length: number
-): number[] => {
-  if (isNumber(itemSizes)) {
-    return Array.from({ length }, () => itemSizes);
-  }
-  if (itemSizes instanceof Array) {
-    if (itemSizes.length === 0) {
-      return Array.from({ length }, () => 0);
-    }
-    if (itemSizes.length < length) {
-      return Array.from({ length }, () => 0).map((_, idx) => {
-        return itemSizes[idx] ?? 0;
-      });
-    }
-    return itemSizes;
-  }
-  return [];
-};
+export { getStyleKey, getSiblingSizes, formatItemSizes };
 
 export const toPercent = (pixel: number, denominator: number): number => {
   if (pixel === 0) return 0;
